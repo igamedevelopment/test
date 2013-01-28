1d89bd733bf03a6870f9493f8fcd10625dbb3f47
smorenoe <sebastianmorenoe@gmail.com>
2013-01-28 16:55:36 +0100

    first test

---
+++ git/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by Sebastian Moreno on 1/28/13.
+//  Copyright (c) 2013 Sebastian Moreno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ git/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by Sebastian Moreno on 1/28/13.
+//  Copyright (c) 2013 Sebastian Moreno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
